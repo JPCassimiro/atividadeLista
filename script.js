8c51798f7453f8c6169055d9f4d18c12cfe17bb1
romulo-souza <romulosouza@alunos.utfpr.edu.br>
2024-05-24 17:16:29 -0300

    alterando função verifica

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+
+//realiza verificação assim que a pagina é carregada
+window.onload = function () {
+    verifica();
+};
+
 let myNodelist = document.getElementsByTagName("li");
+
 for (let i = 0; i < myNodelist.length; i++) {
     let span = document.createElement("span");
     let txt = document.createTextNode("\u00D7"); //caracter x
@@ -6,20 +13,10 @@ for (let i = 0; i < myNodelist.length; i++) {
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
 }
-let close = document.getElementsByClassName("close");
 
-
-for (let i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-        let div = this.parentElement;
-        div.style.display = "none";
-        verifica();
-    }
-}
-
-let list = document.querySelector('ul');
+let list = document.querySelector("ul");
 list.addEventListener('click', function (ev) {
-    if (ev.target.tagName === 'LI') {
+    if (ev.target.tagName === 'LI') { 
         ev.target.classList.toggle('checked');
     }
 }, false);
@@ -29,10 +26,11 @@ function addElemento() {
     let inputValue = document.getElementById("tarefa").value.toUpperCase();
     let t = document.createTextNode(inputValue);
     li.appendChild(t);
-    if (inputValue === '') {
+    if (inputValue === "") {
         alert("Você precisa descrever a tarefa");
     } else {
         document.getElementById("itemLista").appendChild(li);
+        verifica();
     }
     document.getElementById("tarefa").value = "";
     let span = document.createElement("SPAN");
@@ -40,36 +38,37 @@ function addElemento() {
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
+    let close = document.getElementsByClassName("close");
     for (let i = 0; i < close.length; i++) {
         close[i].onclick = function () {
             let div = this.parentElement;
-            div.style.display = "none";
-        }
+            div.remove();
+            verifica();
+        };
     }
-    verifica();
-
 }
 
-function limparLista(){
+function limparLista() {
     let lista = document.getElementById("itemLista");
-    if(lista.innerHTML.length != true){
-        console.log(lista.innerHTML.length)
-        lista.innerHTML = '';
+    if (lista.innerHTML.length != 0) {
+        console.log(lista.innerHTML.length);
+        lista.innerHTML = "";
         console.log("limpou");
     }
     verifica();
 }
 
-function verifica(){
-    console.log("verificação inicio")
+function verifica() {
+    console.log("verificação inicio");
 
     let lista = document.getElementById("itemLista");
-    let botao = document.getElementById("botaoLimpar")
-    if(lista.innerHTML.length == 5 || lista.innerHTML.length == 0){
-        botao.style.cursor = "not-allowed"
-        botao.style.opacity = "0.6"
+    let botao = document.getElementById("botaoLimpar");
+    if (lista.innerHTML.length == 5 || lista.innerHTML.length == 0) {
+        botao.style.pointerEvents = "none"; //desabilita todos os eventos de ponteiro para o elemento
+        botao.style.opacity = "0.6";
     }else{
-        botao.style.opacity = "1"
-        botao.style.cursor = "pointer"
+        botao.style.pointerEvents = "all"; //habilita eventos de ponteiro para o elemento
+        botao.style.opacity = "1";
+        botao.style.cursor = "pointer";
     }
 }
